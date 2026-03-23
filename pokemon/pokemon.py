3d5e4a40ee1655c47d18f1d8f5f5d8f51e867233
Sven Kreienbrock <sven.kreienbrock@ruhr-uni-bochum.de>
2026-03-23 14:26:30 +0100

    Änderung

--- pokemon/pokemon.py
+++ pokemon/pokemon.py
@@ -11,41 +11,61 @@ class Pokemon:
         self.dmg2 = dmg2
         self.dmg3 = dmg3
 
-Pokemon1 = Pokemon("Pflanze", "Tackle",8, "Rankenhieb",12,"Slam",15, "Bisasam",100)
+atk ={"Pflanze":["Blattwerk" , "Rankenhieb"],
+    "Feuer": ["Glut","Nitroladung"],
+    "Wasser":["Blubbstrahl","aquaknarre"],
+    "Elektro":["Donnerblitz"],
+    "Flug":["Luftschnitt","Akrobatik","Pikser"],
+    "Kampf":["Kreuzhieb","Fußtritt"],
+    "Normal":["sternenschauer","Klaps","Tackle","Ruckzuckhieb","Slam","Triplette","Mogelhieb","Schlitzer"],
+    "Gestein":["Juwelen kraft","Steinkante","Steinhagel"],
+    "Boden":["Fruststampfer","Dampfwalze","Schaufler"],
+    "Geist":[],
+    "Käfer":[],
+    "Psycho":[],
+    "Gift":[],
+    "Unlicht":[],
+    "Fee":[],
+    "Drache":[],
+    "Stahl":[],
+    "Eis":[],
+    
+
+Pokemon1 = Pokemon("Pflanze", "Blattwerk",10, "Rankenhieb",12,"Slam",14, "Bisasam",100)
 
 Pokemon2 = Pokemon("Feuer", "Glut",10, "Fruststampfer",14,"Säuselstimme",10, "Krokel",100)
 
-Pokemon3 = Pokemon("wasser","Blubbstrahl",16,"Klaps",8,"Aquaknarre",10,"Froxy",100)
+Pokemon3 = Pokemon("Wasser","Blubbstrahl",16,"Klaps",8,"Aquaknarre",10,"Froxy",100)
 
-Pokemon4 = Pokemon("Elektro","Eisenschweif",10,"Ruckzuckhieb",10,"Donnerblitz",12,"Pikachu",100)
+Pokemon4 = Pokemon("Elektro","Eisenschweif",12,"Ruckzuckhieb",10,"Donnerblitz",14,"Pikachu",100)
 
 Pokemon5 = Pokemon("Flug","Akrobatik",12,"Nitroladung",12,"Pikser",6,"Dartignis",100)
 
-Pokemon6 = Pokemon("Kampf","Mogelhieb",6,"Fußtritt",10,"",12,"Machock",100)
+Pokemon6 = Pokemon("Kampf","Mogelhieb",6,"Fußtritt",10,"kreuzhieb",12,"Machock",100)
 
-Pokemon7 = Pokemon("Normal","",,"",,"",,"",100)
+Pokemon7 = Pokemon("Normal","sternenschauer",10,"Tackle",8,"Triplette",12,"Porygon",100)
 
-Pokemon8 = Pokemon("Gestein","",,"",,"",,"",)
+Pokemon8 = Pokemon("Gestein","Steinhagel",12,"Steinkante",12,"Slam",14,"Onix",110)
 
-Pokemon9 = Pokemon("Boden","",,"",,"",,"",)
+Pokemon9 = Pokemon("Boden","Schaufler",10,"Juwelen kraft",10,"dampfwalze",14,"Kaumalat",)
 
-Pokemon10 = Pokemon("Normal","",,"",,"",,"",)
+Pokemon10 = Pokemon("Geist","Spukball",14,"Biss",12,"Juwelen kraft",10,"Zobiris",100)
 
-Pokemon11 = Pokemon("Käfer","",,"",,"",,"",)
+Pokemon11 = Pokemon("Käfer","Attack Befehl",12,"Heilungs Befehl",10,"Käfergebrumm",10,"Honweisel",100)
 
-Pokemon12 = Pokemon("Psycho","",,"",,"",,"",)
+Pokemon12 = Pokemon("Psycho","Psychoshock",14,"Telekinese",12,"Sternenschauer",10,"Psiaugon",100)
 
-Pokemon13 = Pokemon("Gift","",,"",,"",,"",)
+Pokemon13 = Pokemon("Gift","Schlammwoge",12,"Gifthieb",14,"Ruckzuckhieb",10,"Affiti",100)
 
-Pokemon14 = Pokemon("Unlicht","",,"",,"",,"",)
+Pokemon14 = Pokemon("Unlicht","Finsteraura",12,"Neck Strike",14,"Tackle",8,"Galar Geradaks",100)
 
-Pokemon15 = Pokemon("Fee","",,"",,"",,"",)
+Pokemon15 = Pokemon("Fee","Diebes Kuss",8,"Luftschnitt",12,"Feenbrise",12,"Togekiss",100)
 
-Pokemon16 = Pokemon("Drache","",,"",,"",,"",)
+Pokemon16 = Pokemon("Drache","Draco Meteor",16,"Drachen klaue",14,"slam",14,"Brutalanda",110)
 
-Pokemon17 = Pokemon("Stahl","",,"",,"",,"",)
+Pokemon17 = Pokemon("Stahl","Kopfnuss",12,"Licht kanone",14,"Erdkräfte",14,"Schilterus",110)
 
-Pokemon18 = Pokemon("Eis","",,"",,"",,"",)
+Pokemon18 = Pokemon("Eis","Blizzard",14,"Eis strahl",14,"Schlitzer",12,"Glaziola",100)
 
 print ("Ein wildes " + Pokemon1.name + " greift dich an")
 print ("Du setzt " + Pokemon2.name + " ein, was soll " + Pokemon2.name + " tun, " + Pokemon2.atk1 + " oder " + Pokemon2.atk2 + "?")
